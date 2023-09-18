b54274bbc2cf738d7407b6686474f5337a3e6261
kydiazpe <kdiazped@emeal.nttdata.com>
2023-09-18 18:31:19 -0500

    Siguiente Commit

--- main.go
+++ main.go
@@ -2,12 +2,25 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/KelyDiaz/godesde0/variables"
+	"runtime"
 )
 
 func main() {
-	estado, texto := variables.ConviertoaTexto(1588)
+	/*estado, texto := variables.ConviertoaTexto(1588)
 	fmt.Println(estado)
-	fmt.Println(texto)
+	fmt.Println(texto)*/
+	if os := runtime.GOOS; os == "Linux." || os == "OS X." {
+		fmt.Println("Esto no es Windows")
+	} else {
+		fmt.Println("Esto es", os)
+	}
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("Esto es", os)
+	case "darwin":
+		fmt.Println("Esto es", os)
+	default:
+		fmt.Printf("%s \n", os)
+	}
 }
