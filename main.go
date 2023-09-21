3560d2c4796a91d91f4a92751922a1e9b7b5bc33
kydiazpe <kdiazped@emeal.nttdata.com>
2023-09-21 17:15:45 -0500

    Siguiente Commit

--- main.go
+++ main.go
@@ -1,31 +1,32 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/KelyDiaz/godesde0/ejercicios"
+	"github.com/KelyDiaz/godesde0/teclado"
 )
 
 func main() {
 	/*estado, texto := variables.ConviertoaTexto(1588)
-	fmt.Println(estado)
-	fmt.Println(texto)
-	if os := runtime.GOOS; os == "Linux." || os == "OS X." {
-		fmt.Println("Esto no es Windows")
-	} else {
-		fmt.Println("Esto es", os)
-	}
+		fmt.Println(estado)
+		fmt.Println(texto)
+		if os := runtime.GOOS; os == "Linux." || os == "OS X." {
+			fmt.Println("Esto no es Windows")
+		} else {
+			fmt.Println("Esto es", os)
+		}
 
-	switch os := runtime.GOOS; os {
-	case "linux":
-		fmt.Println("Esto es", os)
-	case "darwin":
-		fmt.Println("Esto es", os)
-	default:
-		fmt.Printf("%s \n", os)
-	}*/
-
-	numero, texto := ejercicios.ConvNumerico("500")
-	fmt.Println(numero)
-	fmt.Println(texto)
+		switch os := runtime.GOOS; os {
+		case "linux":
+			fmt.Println("Esto es", os)
+		case "darwin":
+			fmt.Println("Esto es", os)
+		default:
+			fmt.Printf("%s \n", os)
+		}
+	//---------------
+		numero, texto := ejercicios.ConvNumerico("500")
+		fmt.Println(numero)
+		fmt.Println(texto)
+	//---------------------
+	*/
+	teclado.IngresoNumeros()
 }
